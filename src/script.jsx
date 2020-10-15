db371c325deb6642f2fffec91e0547cbb8328dc1
Marcin <marcin.sudol@outlook.com>
2020-10-15 11:31:38 +0200

    keyboard input listeners

--- src/script.jsx
+++ src/script.jsx
@@ -3,46 +3,55 @@ const clips = [
     {
         id: 1,
         value: "Q",
+        text: "Chord 1",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
     },
     {
         id: 2,
         value: "W",
+        text: "Chord 2",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"
     },
     {
         id: 3,
         value: "E",
+        text: "Chord 3",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"
     },
     {
         id: 4,
         value: "A",
+        text: "Drum 1",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
     },
     {
         id: 5,
         value: "S",
+        text: "Drum 2",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"
     },
     {
         id: 6,
         value: "D",
+        text: "Drum 3",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"
     },
     {
         id: 7,
         value: "Z",
+        text: "Sound 1",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
     },
     {
         id: 8,
         value: "X",
+        text: "Sound 2",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
     },
     {
         id: 9,
         value: "C",
+        text: "Sound 3",
         audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"
     }
 ]
@@ -64,21 +73,43 @@ class DrumPad extends React.Component {
         super(props);
 
         this.play = this.play.bind(this);
+        this.handleKeyPressed = this.handleKeyPressed.bind(this);
     };
 
     static propTypes = {
         id: PropTypes.number.isRequired,
         value: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired,
         audioSrc: PropTypes.string.isRequired,
         onClick: PropTypes.func
     };
 
     play = () => {
-        this.props.onClick(this.props.id);
+        this.props.onClick(this.props.text);
         // OPTIONAL PLAYING AUDIO USING REF
         // this.audioRef.play();
-        document.getElementById(this.props.value).play();
+        let audio = document.getElementById(this.props.value);
+        audio.currentTime = 0;
+        audio.play();
     };
+
+    handleKeyPressed(event) {
+        if (event.code === ("Key" + this.props.value)) {
+            event.preventDefault();
+            document.getElementById("drum-pad" + this.props.id)
+                .click();
+        }
+    }
+
+    // MAYBE MOVE LISTENERS TO MAIN APP
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyPressed);
+    }
+
+    // MAYBE MOVE LISTENERS TO MAIN APP
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.andleKeyPressed);
+    }
 
     render() {
         return (
@@ -122,8 +153,8 @@ class DrumMachine extends React.Component {
         clips: PropTypes.array
     };
 
-    updateDisplay = (id) => {
-        this.setState({ text: id });
+    updateDisplay = (text) => {
+        this.setState({ text });
     };
 
     render() {
